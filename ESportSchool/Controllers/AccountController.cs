ff4af9edb32ff94362762737c15a6a0a1fd2e372
nikita <gudar23@gmail.com>
2021-01-27 18:16:28 +0200

    init

---
+++ ESportSchool/Controllers/AccountController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ESportSchool.Domain.Entities;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using ESportSchool.Services;
+using ESportSchool.Web.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+
+namespace ESportSchool.Web.Controllers
+{
+    [ApiController]
+    [Route("api/account")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly UserService _userService;
+
+        public AccountController(UserService userService, IConfiguration configuration)
+        {
+            _userService = userService;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] AuthorizationViewModel model)
+        {
+            var token = await Authenticate(model);
+            if (token == null)
+            {
+                return BadRequest(new {errorText = "Invalid email or password."});
+            }
+
+            return Ok(token);
+        }
+
+        [HttpPost]
+        [Route("sign-up")]
+        public async Task<IActionResult> SignUp([FromBody] AuthorizationViewModel model)
+        {
+            var user = await _userService.GetUserAsync(model.Email);
+            if (user == null)
+            {
+                user = new User()
+                {
+                    Email = model.Email,
+                    Password = model.Password
+                };
+                await _userService.CreateNewUserAsync(user);
+                var token = await Authenticate(model);
+                if (token == null)
+                {
+                    throw new Exception("Can not authenticate user.");
+                }
+
+                return Ok(token);
+            }
+
+            return BadRequest(new {errorText = "User with specified email already exist."});
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        private async Task<string> Authenticate(AuthorizationViewModel model)
+        {
+            var user = await _userService.GetUserIfVerified(model.Email, model.Password);
+            if (user == null) return null;
+
+            var provider = new JwtProvider(_configuration);
+            var token = provider.GenerateJwtToken(user);
+
+            return await Task.FromResult(token);
+        }
+
+    }
+}
---
+++ ESportSchool/Controllers/ProfileController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ESportSchool.Domain.Entities;
+using ESportSchool.Services;
+using ESportSchool.Web.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace ESportSchool.Web.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/profile")]
+    public class ProfileController : ControllerBase
+    {
+        private readonly UserService _userService;
+        private readonly TeamService _teamService;
+        
+        public ProfileController(UserService userService, TeamService teamService)
+        {
+            _userService = userService;
+            _teamService = teamService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Me()
+        {
+            return Ok(User.Identity.Name);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCoachProfile([FromBody] CoachProfileViewModel model)
+        {
+            var user = await _userService.GetUserAsync(User.Identity.Name);
+            var profile = new CoachProfile
+            {
+                CsgoProfile = model.CsgoProfile,
+                DotaProfile = model.DotaProfile,
+                LolProfile = model.LolProfile,
+                Language = model.Language,
+                User = user,
+            };
+            await _userService.CreateCoachAccountAsync(profile);
+            return Ok();
+        }
+        
+        [HttpPost]
+        [Route("modify")]
+        public async Task ModifyProfile(User user)
+        {
+            await _userService.UpdateUserAsync(user);
+        }
+
+        [HttpGet]
+        [Route("team")]
+        public async Task<IActionResult> PickTeam([FromQuery]string teamName)
+        {
+            var trainings = await _teamService.GetByName(teamName);
+            if (trainings == null)
+            {
+                return BadRequest(new {errorText = "No teams with specified name"});
+            }
+
+            return Ok(trainings);
+        }
+        
+        [HttpPost]
+        [Route("team")]
+        public async Task PickTeam([FromBody] Team team)
+        {
+            var user = await _userService.GetUserAsync(User.Identity.Name);
+            user.Team = team;
+            await _userService.UpdateUserAsync(user);
+        }
+
+        [HttpPost]
+        [Route("schedule")]
+        public async Task<IActionResult> SetSchedule([FromBody] List<ScheduleInterval> schedule)
+        {
+            var user = await _userService.GetUserAsync(User.Identity.Name);
+            if (user?.CoachProfile == null)
+            {
+                return BadRequest(new {errorText = "Your should create coach account first."});
+            }
+            user.CoachProfile.Schedule = schedule;
+            await _userService.UpdateUserAsync(user);
+            return Ok();
+        }
+    }
+}
